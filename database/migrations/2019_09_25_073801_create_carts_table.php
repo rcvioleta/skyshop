c12624824431871f19706b513fd308ce831e6c9b
Rogene Cris Violeta <rcvioleta13@gmail.com>
2019-09-25 18:24:18 +0800

    Auth created, JWT installed, Exception handling. User, Product, Cart controller, migration and model.

---
+++ database/migrations/2019_09_25_073801_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('product_id')->unsigned()->index();
+            $table->integer('quantity');
+            $table->integer('total');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
